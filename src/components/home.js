fb2ccd45d2f848afa0734dd77b5dcaad7f07e5b5
Shubhra1028 <shubhra1028@gmail.com>
2020-03-26 19:25:42 +0530

    routes added

---
+++ src/components/home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Sign up as</h1>
+                <ul>
+                    <li>company</li>
+                    <li>university</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Home;
