74c87e22a1df2364369426bf20847bf7f96e6e1b
Shubhra1028 <shubhra1028@gmail.com>
2020-03-27 17:11:34 +0530

    raoute files

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect} from 'react-router-dom'
 import HomePage from './HomePage/HomePageComponent';
 import SignUp from "./SignUpPage/SignUp";
 import Login from "./LoginPage/Login";
+import Company from "./company/company"
 
 function MainComponent() {
     
@@ -13,6 +14,8 @@ function MainComponent() {
                     <Route exact path="/signup/company" component={ () => <SignUp business />} />
                     <Route exact path="/signup/institute" component={ () => <SignUp institute />} />
                     <Route exact path="/login" component={ () => <Login /> } />
+                    <Route exact path="/company" component={ Company } />
+                    <Route exact path="/institute" component={ Company } />
                     <Redirect to="/home" />
                 </Switch>
             
